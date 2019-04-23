75c32b3863c4d32dfeff1c560ed7ca8d9277df1f
Dam <d4m9704@gmail.com>
2019-04-24 05:29:07 +0900

    버튼이미지추가

--- app/src/main/java/com/example/myapplication/contentActivity.java
+++ app/src/main/java/com/example/myapplication/contentActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,7 +18,7 @@ public class contentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_content);
 
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         TextView tv1 = (TextView) findViewById(R.id.tv1) ;
         String morning = intent.getStringExtra("contact_morning") ;
         if (morning != null)
@@ -33,6 +35,17 @@ public class contentActivity extends AppCompatActivity {
         String evening = intent3.getStringExtra("contact_evening") ;
         if (evening != null)
             tv3.setText(evening) ;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(intent3);
+
+            }
+        };
+        Button btnback = (Button) findViewById(R.id.back);
+        btnback.setOnClickListener(listener);
 
 
     }
