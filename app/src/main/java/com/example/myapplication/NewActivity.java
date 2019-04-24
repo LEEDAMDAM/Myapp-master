ab331eaeb3922778ce61c36bb0cb712ac859c1e2
Pheejung <d4m9704@gmail.com>
2019-04-24 16:35:21 +0900

    SharedPreference 도전

--- app/src/main/java/com/example/myapplication/NewActivity.java
+++ app/src/main/java/com/example/myapplication/NewActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +9,21 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class NewActivity extends AppCompatActivity {
+        SharedPreferences sp;
+
+        EditText editTT;
+        Button btnSAVE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+
+        initPreferences();
+        initView();
+        btn_Event();
+        loadState();
+
 
         Button buttonmoring = (Button)findViewById(R.id.save);
         buttonmoring.setOnClickListener(new View.OnClickListener() {
@@ -30,4 +41,33 @@ public class NewActivity extends AppCompatActivity {
 
 
     }
+
+    private void btn_Event() {
+
+        btnSAVE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveState();
+            }
+        });
+    }
+
+    private void loadState() {
+        editTT.setText(sp.getString("morning",""));
+    }
+    private void saveState(){
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("morning",editTT.getText().toString());
+        editor.commit();
+
+    }
+    private void initPreferences() {
+
+    }
+
+    private void initView() {
+        editTT=(EditText)findViewById(R.id.edit1);
+    }
+
+
 }
